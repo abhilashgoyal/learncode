a01e5583e1d872e87e9b0398990c5a9f0848c503
abhilash <abhilashgoyal4@gmail.com>
2020-04-05 23:06:31 +0530

    Week 1 Day 5 : https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3287/

---
+++ src/test/java/leetcode/ThirtyDayChallange/Day4MoveZerosTest.java
@@ -0,0 +1,12 @@
+package leetcode.ThirtyDayChallange;
+
+
+import org.junit.Test;
+
+public class Day4MoveZerosTest {
+    Day4MoveZeros day4MoveZeros = new Day4MoveZeros();
+
+    @Test
+    public void moveZeroes() {
+    }
+}
