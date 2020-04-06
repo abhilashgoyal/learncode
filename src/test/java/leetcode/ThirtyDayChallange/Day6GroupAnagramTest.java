a67f1ab5a98894b297f003828a4ddad5f77dd3d5
abhilash <abhilashgoyal4@gmail.com>
2020-04-06 23:09:11 +0530

    Week 1 Day 6 : https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3288/

---
+++ src/test/java/leetcode/ThirtyDayChallange/Day6GroupAnagramTest.java
@@ -0,0 +1,18 @@
+package leetcode.ThirtyDayChallange;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class Day6GroupAnagramTest {
+    Day6GroupAnagram ob = new Day6GroupAnagram();
+
+    @Test
+    public void groupAnagrams() {
+        String[] input1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = ob.groupAnagrams(input1);
+        Assert.assertTrue(true);
+    }
+}
